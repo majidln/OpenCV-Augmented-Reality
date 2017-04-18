5515a8835cd7b925bd71bddd6839751ff477c6c6
majidln <majidlotfinia12@yahoo.com>
2017-04-18 12:15:52 +0430

    add open cv to project

--- app/src/main/java/com/example/majid/ar/MainActivity.java
+++ app/src/main/java/com/example/majid/ar/MainActivity.java
@@ -3,11 +3,21 @@ package com.example.majid.ar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+
 public class MainActivity extends AppCompatActivity {
+
+    static {
+        OpenCVLoader.initDebug();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Mat m = new Mat(100, 100, CvType.CV_8UC4);
     }
 }
